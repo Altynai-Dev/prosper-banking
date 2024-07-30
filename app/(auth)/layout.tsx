8b9115bfbd093fc6c0040e22634fb8d55234b35f
Altynai-Dev <altynai.imnl@gmail.com>
2024-07-30 09:05:14 +0600

    finalize the app

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 
 export default function RootLayout({
     children,
@@ -8,11 +7,6 @@ export default function RootLayout({
     return (
       <main className="flex min-h-screen w-full justify-between font-inter">
           {children}
-          <div className="auth-asset">
-            <div>
-              <Image src="/icons/auth-image.svg" alt="auth" width={500} height={500} />
-            </div>
-          </div>
       </main>
     );
   }
